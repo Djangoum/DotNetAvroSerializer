19d05385c75fe8f81f15bdc3af35bd848f6a2355
Ariel Amor García <arielamorgarcia@gmail.com>
2023-08-23 22:08:54 +0200

    Add AvroField attribute to override records schemas field name (#12)
    * Implement equality chain of SerializableTypeMetadata derived types
    
    * Implement the posibility to override record names with AvroField attribute
    
    * Change write files to .write.g.cs extension
    
    * Remove using

---
+++ tests/AvroSerializer.Write.Tests/Models/RecordWithOverridenNames.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetAvroSerializer.Write.Tests.Models
+{
+    public class RecordWithOverridenNames
+    {
+        [AvroField("matching_name")]
+        public required string NonMatchingName { get; set; }
+    }
+}

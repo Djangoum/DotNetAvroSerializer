68990213a18cf5e088c870f5176ff771d5b187c0
Ariel Amor <95054179@lidl.de>
2023-11-05 23:29:27 +0100

    Allow inheritance

--- src/DotNetAvroSerializer.Generators/Models/RecordSerializableTypeMetadata.cs
+++ src/DotNetAvroSerializer.Generators/Models/RecordSerializableTypeMetadata.cs
@@ -9,11 +9,21 @@ internal class RecordSerializableTypeMetadata : SerializableTypeMetadata
     public RecordSerializableTypeMetadata(ITypeSymbol typeSymbol, Compilation compilation)
         : base(typeSymbol)
     {
-        Fields = typeSymbol
-                .GetMembers()
-                .Where(s => s.Kind is SymbolKind.Property)
-                .Cast<IPropertySymbol>()
-                .Select(t => new FieldSerializableTypeMetadata(From(t.Type, compilation), t, t.Name));
+        Fields = GetBaseTypesAndThis(typeSymbol).SelectMany(typeSymbol => typeSymbol
+            .GetMembers()
+            .Where(s => s.Kind is SymbolKind.Property && s.DeclaredAccessibility == Accessibility.Public)
+            .Cast<IPropertySymbol>()
+            .Select(t => new FieldSerializableTypeMetadata(From(t.Type, compilation), t, t.Name)));
+    }
+
+    private static IEnumerable<ITypeSymbol> GetBaseTypesAndThis(ITypeSymbol type)
+    {
+        var current = type;
+        while (current != null)
+        {
+            yield return current;
+            current = current.BaseType;
+        }
     }
 
     protected override SerializableTypeKind Kind => SerializableTypeKind.Record;
